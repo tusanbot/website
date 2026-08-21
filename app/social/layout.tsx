c8223338ac391ffeec8ca4c6c616581ee3278914
tusanbot <mshmahyar9@gmail.com>
2026-08-21 21:03:32 +0330

    Add shared footer to social services pages

--- app/social/layout.tsx
+++ app/social/layout.tsx
@@ -1,12 +1,14 @@
 import type { ReactNode } from "react";
 import SocialNavigationGuard from "./SocialNavigationGuard";
 import SocialPricingDisplay from "./SocialPricingDisplay";
+import HomeFooter from "@/components/home/HomeFooter";
 
 export default function SocialLayout({ children }: { children: ReactNode }) {
     return (
         <SocialNavigationGuard>
             <div dir="rtl" className="min-h-screen bg-[var(--background)] text-[var(--text)]">
                 {children}
+                <HomeFooter />
                 <SocialPricingDisplay />
             </div>
         </SocialNavigationGuard>
