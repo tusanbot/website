2b9153f8efe869949fff62587985923c372e1975
tusanbot <mshmahyar9@gmail.com>
2026-09-04 21:22:03 +0330

    feat(android): add TWA app module

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,8 @@
+plugins { id("com.android.application") }
+
+android { namespace = "ir.tusancn.app"; compileSdk = 35
+    defaultConfig { applicationId = "ir.tusancn.app"; minSdk = 23; targetSdk = 35; versionCode = 1; versionName = "1.0.0" }
+    buildTypes { release { isMinifyEnabled = false; proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro") } }
+}
+
+dependencies { implementation("com.google.androidbrowserhelper:androidbrowserhelper:2.7.3") }
